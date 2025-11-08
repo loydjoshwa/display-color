61a0090371f9d64e740cfce5b97cccc5ea41baba
loyd <loydjoshwad@gmail.com>
2025-11-08 20:28:47 +0530

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+import Header from "./components/header.jsx"
+
+export default function App(){
+
+return (
+  <Header />
+)
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+import "./header.css"
+function Header() {
+   const [color,setColor]=useState();
+   function changed(e){
+    setColor(e.target.value)
+   }
+ 
+  return(
+    <div className="container">
+      <h1>Display Color</h1>
+      <div className="firstdiv">
+       
+        <input type="text" name="text here"  placeholder=" Type color here" value={color} onChange={changed}/>
+      </div>
+      <div className="box" style={{backgroundColor:color}}>
+
+      </div>
+    </div>
+  )
+}
+export default Header
